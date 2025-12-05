c4b71e4c49a013f4692244a17643f4fe26fd0a09
Aaron Dill <aaronsacks2006@gmail.com>
2025-12-05 03:12:06 -0600

    chore: boilerplate

---
+++ src/day1.rs
@@ -0,0 +1,4 @@
+#[aoc(day1, part1)]
+pub fn part1(input: &str) -> String {
+    return format!("Yep! {}", input.len());
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,10 @@
+// Make all the things work
+extern crate aoc_runner;
+#[macro_use]
+extern crate aoc_runner_derive;
+
+// List all the modules
+pub mod day1;
+
+// Actually work :)
 aoc_lib! { year = 2025 }
