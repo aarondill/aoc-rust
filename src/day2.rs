7cd43a4ea184f7c94a77cdbfee012e0794a3d73a
Aaron Dill <aaronsacks2006@gmail.com>
2025-12-07 04:19:45 -0600

    feat: day2

--- src/day2.rs
+++ src/day2.rs
@@ -1,24 +1,62 @@
-#[aoc(day2, part1)]
-fn part1(input: &str) -> usize {
-    // todo!("Part 1")
-    return input.len();
+#[aoc_generator(day2)]
+fn parse(input: &str) -> Vec<(usize, usize)> {
+    input
+        .split(',')
+        .filter(|s| !s.is_empty())
+        .map(|range| {
+            let mut range = range.split('-');
+            let start = range
+                .next()
+                .expect("range is missing start")
+                .parse::<usize>()
+                .expect("start is not a number");
+            let end = range
+                .next()
+                .expect("range is missing end")
+                .parse::<usize>()
+                .expect("end is not a number");
+            assert!(range.next().is_none(), "range has too many dashes");
+            assert!(start <= end, "start must be less than or equal to end");
+            (start, end)
+        })
+        .collect()
 }
 
-// #[aoc(day2, part2)]
-// fn part2(input: &str) -> String {
-//     todo!()
-// }
+#[aoc(day2, part1)]
+// Input is a single string of comma-separated ranges of numbers.
+fn part1(input: &Vec<(usize, usize)>) -> usize {
+    fn is_invalid(num: usize) -> bool {
+        let str = num.to_string();
+        let (left, right) = str.split_at(str.len() / 2);
+        left == right
+    }
+    input.iter().flat_map(|range| (range.0..=range.1).filter(|&i| is_invalid(i))).sum()
+}
+
+#[aoc(day2, part2)]
+fn part2(input: &Vec<(usize, usize)>) -> usize {
+    fn is_invalid(num: usize) -> bool {
+        let str = num.to_string();
+        // Not the most efficient way (this is basically a palindrome problem), but it works
+        (1..=str.len() / 2).rev().any(|len| {
+            let n = str.len() / len;
+            let left = &str[..len];
+            str == left.repeat(n)
+        })
+    }
+    input.iter().flat_map(|range| (range.0..=range.1).filter(|&i| is_invalid(i))).sum()
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    const INPUT: &str = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,\n1698522-1698528,446443-446449,38593856-38593862,565653-565659,\n824824821-824824827,2121212118-2121212124";
+    const INPUT: &str = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
     #[test]
     fn test_part1() {
-        assert_eq!(part1(INPUT), 1227775554);
+        assert_eq!(part1(&parse(INPUT)), 1227775554);
     }
-    // #[test]
-    // fn test_part2() {
-    //     assert_eq!(part2(INPUT), 6);
-    // }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(&parse(INPUT)), 4174379265);
+    }
 }
