4022e477d0712d5929d581c8ff787416b070579a
Aaron Dill <aaronsacks2006@gmail.com>
2025-12-08 02:42:22 -0600

    feat: day4 part2
    also refactors part1 since they use much of the same logic

--- src/day4.rs
+++ src/day4.rs
@@ -24,36 +24,56 @@ fn parse(input: &str) -> Input {
         .collect::<Vec<_>>()
         .into()
 }
+// Returns true if the given position is removable
+fn is_removeable(input: &Input, (y, x): (usize, usize)) -> bool {
+    let paper_adjacent = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+        .iter()
+        .map(|(dy, dx)| (y as isize + dy, x as isize + dx))
+        .filter_map(|pos| match pos {
+            (y, x) if x < 0 || y < 0 => None,
+            (y, x) if x >= input.cols() as isize || y >= input.rows() as isize => None,
+            (y, x) => Some((y as usize, x as usize)),
+        })
+        .filter(|&pos| input[pos] == Space::Paper)
+        .count();
+    paper_adjacent < 4
+}
+// Returns an iterator over the coordinates of removable Papers
+fn removeable_iter(input: &Input) -> impl Iterator<Item = (usize, usize)> {
+    input
+        .indexed_iter()
+        .filter_map(|(pos, &space)| match space {
+            Space::Paper => Some(pos),
+            _ => None,
+        })
+        .filter(|&pos| is_removeable(input, pos))
+}
 
 #[aoc(day4, part1)]
 fn part1(input: &Input) -> usize {
-    input
-        .indexed_iter()
-        .filter(|&(_, &space)| space == Space::Paper)
-        .filter(|&((y, x), _)| {
-            let paper_adjacent = (-1..=1)
-                .into_iter()
-                .flat_map(|dy| (-1..=1).into_iter().map(move |dx| (dy, dx)))
-                .filter(|&(dy, dx)| dx != 0 || dy != 0)
-                .map(|(dy, dx)| (y as isize + dy, x as isize + dx))
-                .filter_map(|(y, x)| {
-                    if x < 0 || y < 0 {
-                        return None;
-                    }
-                    let (y, x) = (y as usize, x as usize);
-                    if x < input.cols() && y < input.rows() { Some((y, x)) } else { None }
-                })
-                .filter(|&pos| input[pos] == Space::Paper)
-                .count();
-            paper_adjacent < 4
-        })
-        .count()
+    // Just count the number of removable Papers
+    removeable_iter(input).count()
 }
 
-// #[aoc(day4, part2)]
-// fn part2(input: &Input) -> u64 {
-//     todo!()
-// }
+#[aoc(day4, part2)]
+fn part2(input: &Input) -> u64 {
+    // the aoc macro doesn't support mutable references, so we have to clone
+    let input = &mut input.clone();
+    // Continue removing Papers until there are no more removable Papers
+    let mut count = 0;
+    loop {
+        // We *must* evaluate the full iterator before we mutate the input
+        let removable = removeable_iter(input).collect::<Vec<_>>();
+        if removable.is_empty() {
+            break; // no do-whiles here
+        }
+        count += removable.len() as u64;
+        for pos in removable {
+            input[pos] = Space::Empty;
+        }
+    }
+    count
+}
 
 #[cfg(test)]
 mod tests {
@@ -73,8 +93,8 @@ mod tests {
     fn test_part1() {
         assert_eq!(part1(&parse(INPUT)), 13);
     }
-    // #[test]
-    // fn test_part2() {
-    //     assert_eq!(part2(&parse(INPUT)), 0);
-    // }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(&parse(INPUT)), 43);
+    }
 }
